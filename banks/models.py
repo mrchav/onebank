79288282079f47d98ea518c200563f611fbd5716
admchav <admchav@gmail.com>
2022-03-20 19:10:05 +0300

    добавил первые банки настроил вывод логотипов

--- banks/models.py
+++ banks/models.py
@@ -7,15 +7,15 @@ class Bank(models.Model):
     bank_adress_index = models.CharField(max_length=250, verbose_name='Индекс офиса', null=True)
     bank_main_adress = models.CharField(max_length=250, verbose_name='Главный офис', null=True)
     bank_phone = models.CharField(max_length=20, verbose_name='Телефон')
-    bank_short_desc = models.CharField(max_length=250,verbose_name='Описание банка', null=True)
+    bank_short_desc = models.CharField(max_length=250,verbose_name='Описание банка', null=True, blank=True)
     bank_site_url = models.URLField(verbose_name='адрес сайта')
-    bank_logo = models.ImageField(verbose_name='Логотип банка')
-    bank_license = models.IntegerField (verbose_name='Номер лицензии')
-    bank_off_count = models.IntegerField(verbose_name='Кол-во отделений в России')
-    bank_fin_deposit = models.IntegerField(verbose_name='Вклады/депозиты', null=True)
-    bank_fin_assets = models.IntegerField(verbose_name='активы', null=True)
-    bank_fin_fiz_credit = models.IntegerField(verbose_name='Кредиты физ лицам', null=True)
-    bank_fin_profit = models.IntegerField(verbose_name='Прибыль банка', null=True)
+    bank_logo = models.ImageField(verbose_name='Логотип банка', upload_to="bank_logo/")
+    bank_license = models.IntegerField(verbose_name='Номер лицензии')
+    bank_off_count = models.IntegerField(verbose_name='Кол-во отделений в России', null=True, blank=True)
+    bank_fin_deposit = models.BigIntegerField (verbose_name='Вклады/депозиты', null=True, blank=True)
+    bank_fin_assets = models.BigIntegerField (verbose_name='активы', null=True, blank=True)
+    bank_fin_fiz_credit = models.BigIntegerField(verbose_name='Кредиты физ лицам', null=True, blank=True)
+    bank_fin_profit = models.BigIntegerField (verbose_name='Прибыль банка', null=True, blank=True)
 
     def __str__(self):
         return f'{self.pk} | {self.bank_name}'
--- onebank/urls.py
+++ onebank/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+
 from banks import views as banks_views
 from onebank import views as onebank_views
 
@@ -23,4 +26,5 @@ urlpatterns = [
     path('banks/', banks_views.show_banks),
     path('', onebank_views.main_page),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
